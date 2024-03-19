dc8da76002f047fdda5ca56d110233c2b57805e2
x23213167 <159258957+x23213167@users.noreply.github.com>
2024-03-19 16:27:50 +0700

    Update settings.py

--- GMS/settings.py
+++ GMS/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'gym/static')
+
+
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'gym/static'),
